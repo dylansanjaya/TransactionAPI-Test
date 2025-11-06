ac419b81cf9dffe90b372a3da5fb2ed1d1b6ba30
dylansanjaya <dylanhikmasanjaya@gmail.com>
2025-11-06 21:23:00 +0700

    add: congfig vercel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
+import serverless from "serverless-http";
+
+export const handler = serverless(app);
+export default app;
 
 app.listen(env.PORT, () => {
   console.log(`Server running at http://localhost:${env.PORT}`);
